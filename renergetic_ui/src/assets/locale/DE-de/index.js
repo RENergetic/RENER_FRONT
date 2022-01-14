33b90e6974d31c777442c3b4546baf91ebf9d67b
Tomasz Ciesielczyk <tomasz.ciesielczyk@gmail.com>
2022-01-14 21:02:36 +0100

    i18n plugin config

---
+++ renergetic_ui/src/assets/locale/DE-de/index.js
@@ -0,0 +1,11 @@
+import primeview from "./primevue.json";
+import menu from "./menu.json";
+import view from "./view.json";
+
+const de = {
+  // "view": view,
+  menu: menu,
+  view: view,
+  ...primeview,
+};
+export default de;
