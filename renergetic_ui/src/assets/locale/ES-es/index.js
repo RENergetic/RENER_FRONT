92c0d30a5d198a0c07408535fbeee43aeeeddf66
Tomasz Ciesielczyk <tomasz.ciesielczyk@gmail.com>
2022-01-14 21:03:02 +0100

    fix previous commit

--- renergetic_ui/src/assets/locale/ES-es/index.js
+++ renergetic_ui/src/assets/locale/ES-es/index.js
@@ -2,10 +2,10 @@ import primeview from "./primevue.json";
 import menu from "./menu.json";
 import view from "./view.json";
 
-const en = {
+const es = {
   // "view": view,
   menu: menu,
   view: view,
   ...primeview,
 };
-export default en;
+export default es;
--- renergetic_ui/src/assets/locale/PL-pl/index.js
+++ renergetic_ui/src/assets/locale/PL-pl/index.js
@@ -1,9 +1,11 @@
 import primeview from "./primevue.json";
 import menu from "./menu.json";
+import view from "./view.json";
 
 const pl = {
   // "view": view,
   menu: menu,
+  view: view,
   ...primeview,
 };
 export default pl;
