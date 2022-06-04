5d81bae66e687ee2e6cb4e3745b03aefacc6a2bf
Tomasz Ciesielczyk <tomasz.ciesielczyk@gmail.com>
2022-06-04 14:27:48 +0200

    ren roles

--- renergetic_ui/src/plugins/model/Enums.js
+++ renergetic_ui/src/plugins/model/Enums.js
@@ -29,4 +29,24 @@ const NotificationContext = Object.freeze({
   AREA: "area",
   TILE: "area",
 });
-export { Colors, AssetTypes, TileTypes, NotificationContext, HeatMapType };
+
+const RenRoles = Object.freeze({
+  REN_GUEST: 0x00000000,
+  "ren-guest": 0x00000000,
+  REN_VISITOR: 0x00000010,
+  "ren-visitor": 0x00000010,
+  REN_USER: 0x00000100,
+  "ren-user": 0x00000100,
+  REN_MANAGER: 0x00001000,
+  "ren-manager": 0x00001000,
+  REN_TECHNICAL_MANAGER: 0x00010000,
+  "ren-technical-manager": 0x00010000,
+  REN_ADMIN: 0x00100000,
+  "ren-admin": 0x001000000,
+  REN_STAFF: 0x010000000,
+  "ren-staff": 0x010000000,
+  REN_DEV: 0x11111111,
+  "ren-dev": 0x11111111,
+});
+
+export { Colors, AssetTypes, TileTypes, NotificationContext, HeatMapType, RenRoles };
