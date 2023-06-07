c2dc9bb6bcf90e347e56f377b65e41e4dfdc5b18
Tomasz Ciesielczyk <tomasz.ciesielczyk@gmail.com>
2023-06-07 19:22:52 +0200

    conversion bug fix

--- renergetic_ui/src/plugins/renergetic/utils.js
+++ renergetic_ui/src/plugins/renergetic/utils.js
@@ -313,7 +313,7 @@ export default class RenUtils {
         let value = pData.current[m.aggregation_function][m.id];
         let newV = this.app.$store.getters["view/convertValue"](m.type, value, newUnit);
         cp.current[m.aggregation_function][m.id] = newV;
-        if (pData.max) {
+        if (pData.max && pData.max[m.aggregation_function]) {
           let value = pData.max[m.aggregation_function][m.id];
           let newV = this.app.$store.getters["view/convertValue"](m.type, value, newUnit);
           cp.max[m.aggregation_function][m.id] = newV;
