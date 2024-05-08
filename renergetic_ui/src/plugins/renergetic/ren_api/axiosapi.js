a8af17e44cde8931a01e836838b5532265249316
Jeremy W. <jeremy.wautelet@inetum-realdolmen.world>
2024-05-08 11:58:31 +0200

    Splitting base URLs for splitted APIs.

--- renergetic_ui/src/plugins/renergetic/ren_api/axiosapi.js
+++ renergetic_ui/src/plugins/renergetic/ren_api/axiosapi.js
@@ -1,9 +1,11 @@
 export default class AxiosAPI {
-  constructor(axiosInstance, vueInstance, authApi) {
-    this.axios = axiosInstance;
+  constructor(axiosInstances, vueInstance, authApi) {
+    this.axiosInstances = axiosInstances;
     this.vueInstance = vueInstance;
     this.authApi = authApi;
-    this.init();
+    for (const element of this.axiosInstances) {
+      this.init(element);
+    }
   }
   //wrapper for store
   storeCommit(action) {
@@ -27,8 +29,8 @@ export default class AxiosAPI {
   /**
    * init axxios interceptors
    */
-  init() {
-    this.axios.interceptors.request.use(
+  init(axiosInstance) {
+    axiosInstance.interceptors.request.use(
       (req) => {
         if (req.spinner) {
           this.storeCommit("spinner/start");
@@ -56,7 +58,7 @@ export default class AxiosAPI {
       },
     );
 
-    this.axios.interceptors.response.use(
+    axiosInstance.interceptors.response.use(
       (res) => {
         if (res.config.spinner) {
           this.storeCommit("spinner/stop");
