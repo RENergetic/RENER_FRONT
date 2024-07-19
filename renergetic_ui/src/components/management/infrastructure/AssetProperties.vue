f3193c9b86a06c6b1d82d278ac6555e3609e59f9
Jeremy W. <jeremy.wautelet@inetum-realdolmen.world>
2024-07-19 08:57:46 +0200

    Adding latest change to AssetProperties file.

--- renergetic_ui/src/components/management/infrastructure/AssetProperties.vue
+++ renergetic_ui/src/components/management/infrastructure/AssetProperties.vue
@@ -6,11 +6,23 @@
       <!-- <span>{{ detailsKey }}</span>
         <InputText v-model="details[detailsKey]" type="text"></InputText> -->
       <!-- </div> -->
+      <div class="property-form">
+        <h3>Asset Detail Key</h3>
+        <h3>Asset Detail Value</h3>
+      </div>
       <div v-for="(detail, index) in detailsList" :key="index" class="property-form">
-        <InputText v-model="detailsList[index].key" placeholder="Key" @keyup="checkForDuplicatesAndNewLineNeed()" />
-        <InputText v-model="detailsList[index].value" placeholder="Value" />
+        <InputText
+          v-if="!detail.original"
+          v-model="detailsList[index].key"
+          placeholder="Key"
+          @keyup="checkForDuplicatesAndNewLineNeed()"
+          @focusout="deleteIfEmpty(index)"
+        />
+        <div v-else>{{ detailsList[index].key }}</div>
+        <InputText v-model="detailsList[index].value" placeholder="Value" @focusout="deleteIfEmpty(index)" />
         <div v-if="detail.error !== null" class="property-form-error">
-          <span v-if="detail.error === 'dup'">Duplicated</span>
+          <span v-if="detail.error === 'dup'">Key cannot be duplicated</span>
+          <span v-if="detail.error === 'empty'">Key cannot be empty</span>
         </div>
         <!-- <span>{{ detailsKey }}</span>
         <InputText v-model="details[detailsKey]" type="text"></InputText> -->
@@ -39,10 +51,11 @@ export default {
     open(selectedAsset, detailsKeys) {
       this.detailsList = [];
       for (const [key, value] of Object.entries(selectedAsset.details)) {
-        this.detailsList.push({ index: this.detailIndex, key: key, value: value, error: null });
+        this.detailsList.push({ index: this.detailIndex, key: key, value: value, error: null, original: true });
         this.detailIndex += 1;
       }
-      this.detailsList.push({ index: this.detailIndex, key: "", value: "", error: null });
+      this.detailsList.sort((a, b) => a.key.localeCompare(b.key));
+      this.detailsList.push({ index: this.detailIndex, key: "", value: "", error: null, original: false });
       this.detailIndex += 1;
 
       this.detailsKeys = detailsKeys;
@@ -54,12 +67,22 @@ export default {
       );*/
       this.dialog = true;
     },
+    deleteIfEmpty(index) {
+      if (index === this.detailsList.length - 1) {
+        return;
+      }
+
+      var detail = this.detailsList[index];
+      if ((detail.key === null || detail.key.length === 0) && (detail.value === null || detail.value.length === 0)) {
+        this.detailsList.splice(index, 1);
+      }
+    },
     checkForDuplicatesAndNewLineNeed() {
       this.checkForDuplicates();
 
       const last = this.detailsList[this.detailsList.length - 1];
       if (last.key !== null && last.key.length > 0) {
-        this.detailsList.push({ index: this.detailIndex, key: "", value: "", error: null });
+        this.detailsList.push({ index: this.detailIndex, key: "", value: "", error: null, original: false });
         this.detailIndex += 1;
       }
     },
@@ -67,15 +90,18 @@ export default {
       var found = false;
       this.detailsList.forEach((item) => (item.error = null));
 
-      this.detailsList.forEach((itemA) =>
+      this.detailsList.forEach((itemA, index) => {
         this.detailsList.forEach((itemB) => {
           if (itemA.index !== itemB.index && itemA.key !== null && itemA.key.length > 0 && itemA.key === itemB.key) {
             itemA.error = "dup";
             itemB.error = "dup";
             found = true;
           }
-        }),
-      );
+        });
+        if ((itemA.key === null || itemA.key.length === 0) && index !== this.detailsList.length - 1) {
+          itemA.error = "empty";
+        }
+      });
       return found;
     },
     checkForDuplicatesAndRemoveEmpty() {
@@ -83,7 +109,7 @@ export default {
         return false;
       }
 
-      this.detailsList = this.detailsList.filter((item) => item.key !== null && item.key.length > 0);
+      this.detailsList = this.detailsList.filter((item) => (item.key !== null && item.key.length > 0) || item.original);
 
       return true;
     },
@@ -121,6 +147,16 @@ export default {
   column-gap: 1rem;
   margin-bottom: 1rem;
   margin-top: 1rem;
+  & > input {
+    max-width: 100%;
+  }
+  & > h3 {
+    color: #495057;
+  }
+  & > div {
+    display: flex;
+    align-items: center;
+  }
 }
 .property-form-error {
   color: red;
