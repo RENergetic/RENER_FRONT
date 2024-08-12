84353119bacaf91bdbfc167a5509ca2ebe5a8c37
tomasz_ciesielczyk <tomaszc@man.poznan.pl>
2024-08-12 20:15:39 +0200

    locales order

---
+++ renergetic_ui/src/assets/locales copy/DE-de/index.js
@@ -0,0 +1,17 @@
+import primeview from "./primevue.json";
+import menu from "./menu.json";
+import view from "./view.json";
+import model from "./model.json";
+import settings from "./settings.json";
+import enums from "./enums.json";
+
+const de = {
+  // "view": view,
+  menu: menu,
+  enums: enums,
+  view: view,
+  model: model,
+  settings: settings,
+  ...primeview,
+};
+export default de;
